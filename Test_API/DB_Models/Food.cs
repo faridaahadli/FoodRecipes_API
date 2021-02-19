ac4656007f43a0f33a0fd4267bff48bea1c7656d
farida ahadli <faridaahadli69@gamil.com>
2021-02-19 13:04:51 +0400

    project created

---
+++ Test_API/DB_Models/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_API.DB_Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Preparation { get; set; }
+        public string Tools { get; set; }
+        public string Ingredients { get; set; }
+        public int TypeId { get; set; }
+        public virtual Type Type { get; set; }
+        public int CuisineId { get; set; }
+        public virtual Cuisine Cuisine { get; set; }
+      
+    }
+
+}
---
+++ Test_API/DB_Models/FoodRecipeDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_API.DB_Models
+{
+    public class FoodRecipeDB:DbContext
+    {
+        public FoodRecipeDB(DbContextOptions options):base(options)
+        {           
+        }
+        public DbSet<Food> Cuisines { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Food> Foods { get; set; }
+    }
+}
